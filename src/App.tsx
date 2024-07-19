c9043a7dece1747352f5965bf45f5203aad41e17
Paola Andrea Ramirez <129551206+paolandre@users.noreply.github.com>
2024-07-18 22:08:41 -0500

    feat: aviso legal page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home/Home';
-
+import FormPage from './components/FormPage/FormPage';
+import DisclaimerPage from './components/DisclaimerPage/DisclaimerPage';
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/form" element={<FormPage />} />
+          <Route path="/disclaimerPage" element={<DisclaimerPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Disclaimer/Disclaimer.tsx
+++ src/components/Disclaimer/Disclaimer.tsx
@@ -1,9 +1,33 @@
 import React from 'react';
-import './Disclaimer.module.css';
+import { IoArrowBackOutline } from 'react-icons/io5';
+import './Disclaimer';
 
-const Disclaimer: React.FC = () => {
+const Disclaimer = () => {
+    const handleBack = () => {
+        window.history.back();
+    };
+
     return (
-        <div className="disclaimer">
+        <div className="form-page flex flex-col items-center justify-center p-4 relative">
+            <div className="overlay1 absolute inset-0"></div>
+            <div className="overlay2 absolute inset-0"></div>
+            <div className="relative z-10 text-center">
+                <div className="absolute top-4 left-4 cursor-pointer" onClick={handleBack}>
+                    <IoArrowBackOutline className="text-iconColor" size={24} />
+                    <span className="text-iconColor ml-2">Regresar</span>
+                </div>
+                <h1 className="gradient-text title mt-2">
+                    Aviso <span className="title">legal</span>
+                </h1>
+                <p className="text-paragraphColor text-sm font-light">
+                    Este sitio web ofrece apoyo emocional mediante inteligencia artificial dirigido a
+                    programadores y emprendedores. Es importante destacar que los consejos y
+                    recomendaciones proporcionados no sustituyen la asesoría profesional de un psicólogo,
+                    terapeuta o cualquier otro profesional de la salud mental. Si experimentas problemas
+                    graves o persistentes, te recomendamos buscar ayuda profesional. El uso de este sitio
+                    implica la aceptación de estos términos.
+                </p>
+            </div>
         </div>
     );
 };
