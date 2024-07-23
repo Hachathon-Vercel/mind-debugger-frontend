28fad3db37f3731e1dd394a04e2bcf5a00c57331
Paola Andrea Ramirez <129551206+paolandre@users.noreply.github.com>
2024-07-23 00:43:49 -0500

    feat: chat and demo components - initial setup

---
+++ src/components/Demo/DemoPage.tsx
@@ -0,0 +1,14 @@
+import Navbar from '../Navbar';
+import Demo from './Demo';
+
+
+function DemoPage() {
+    return (
+        <div className="min-h-screen flex flex-col">
+            <Navbar />
+            <Demo />
+        </div>
+    );
+}
+
+export default DemoPage;
