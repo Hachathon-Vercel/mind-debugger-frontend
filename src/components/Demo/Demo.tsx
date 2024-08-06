9556190520f46c57cfbc7060b496351943b32fc9
Paola Andrea Ramirez <129551206+paolandre@users.noreply.github.com>
2024-08-06 02:46:16 -0500

    Demo component

--- src/components/Demo/Demo.tsx
+++ src/components/Demo/Demo.tsx
@@ -2,20 +2,35 @@ import React from 'react';
 import { IoArrowBackOutline } from 'react-icons/io5';
 import './Demo.css';
 
-const Demo = () => {
+const Demo: React.FC = () => {
     const handleBack = () => {
         window.history.back();
     };
 
     return (
-        <div className="form-page flex flex-col items-center justify-center p-4 relative">
+        <div className="bg">
             <div className="back-container flex items-center absolute top-4 left-4 cursor-pointer" onClick={handleBack}>
-                <IoArrowBackOutline className="text-iconColor" size={16} />
+                <IoArrowBackOutline className="text-iconColor" size={24} />
                 <span className="text-iconColor ml-2">Regresar</span>
             </div>
-
+            <div className="line-container">
+                <div className="line"></div>
+                <div className="line"></div>
+                <div className="line"></div>
+            </div>
+            <div className="demo-chat-container-wrapper">
+                <div className="demo-chat-container">
+                    <div className="demo-video-responsive">
+                        <video controls>
+                            <source src="/videos/demoVideo.mp4" type="video/mp4" />
+                            Tu navegador no soporta la reproducción de videos.
+                        </video>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
 
 export default Demo;
+
